db7db7ef607264322828a03693bfffdd497e4bdc
nawaf4224 <azzanawaf03@gmail.com>
2024-05-29 21:31:16 +0700

    Projek awal forum diskusi

---
+++ src/Components/LoginForm/LoginForm.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './LoginForm.css';
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+const LoginForm = () => {
+
+  return (
+    <div className='wrapper'>
+      <form action=''>
+        <h1> Login </h1>
+            <div className='input-box'>
+            <input type='text'placeholder='username'required/>
+            <FaUser className='icon'/>
+            </div>
+            <div className='input-box'>
+            <input type='password'placeholder='password'required/>
+            <FaLock className='icon'/>
+            </div>
+
+            <div className="remember-forget">
+              <label><input type="checkbox"/> remember me </label>
+              <a href='#'>forget password?</a>
+            </div>
+
+            <button type='submit'>Login</button>
+
+            <div className="register-link">
+              <p>Don't have an account? <Link to="/RegistrationForm">Register</Link></p>
+            </div>
+      </form>
+
+      
+    </div>
+  )
+}
+
+export default LoginForm
