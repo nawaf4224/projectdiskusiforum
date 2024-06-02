350827c9e127db536d0a67cf49abaf66fa947bc5
Kyroline <rizky.w.dewantoro@gmail.com>
2024-06-02 10:30:20 +0700

    Added simple session authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
 import React from 'react'
 import AppRouter from './router/AppRouter'
+import { UserProvider } from './contexts/UserContext'
 
 const App = () => {
-    return <AppRouter />
+    return (
+        <UserProvider>
+            <AppRouter />
+        </UserProvider>
+    )
 }
 
 export default App;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import useSessionStorage from '../lib/useSessionStorage'
 import { Outlet } from 'react-router-dom'
+import axiosInstance from '../lib/axiosInstance'
+import { UserContext } from '../contexts/UserContext'
 
 const MainLayout = () => {
+    const navigate = useNavigate()
+    const apiKey = useSessionStorage('api_key')
+    const { setUser } = useContext(UserContext)
+
+    const getUser = async () => {
+        try {
+            let response = await axiosInstance.get('v1/auth/validate').data
+            setUser(response.data)
+        } catch (error) {
+            setUser(null)
+        }
+    }
+
+    useEffect(() => {
+        if (!apiKey)
+            navigate('/login')
+    }, [apiKey, navigate])
+
+    useEffect(() => {
+        if (apiKey)
+            getUser()
+
+        return (() => {
+            setUser(null)
+        })
+    }, [])
+
     return (
         <>
-        {/* Mungkin sini dikasih sidebar */}
-        <Outlet /> {/* Ini isi dari pagenya */}
-        {/* Mungkin sini dikasih sidebar kanan yg isinya popular people / your commuities */}
+            {/* Mungkin sini dikasih sidebar */}
+            <Outlet /> {/* Ini isi dari pagenya */}
+            {/* Mungkin sini dikasih sidebar kanan yg isinya popular people / your commuities */}
         </>
     )
 }
